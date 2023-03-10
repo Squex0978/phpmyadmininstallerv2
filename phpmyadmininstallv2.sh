6f968fdf696c107d7c48715be502d228a3be604f
Squex0978 <123485001+Squex0978@users.noreply.github.com>
2023-03-10 12:11:04 +0100

    Update phpmyadmininstallv2.sh

--- phpmyadmininstallv2.sh
+++ phpmyadmininstallv2.sh
@@ -1,55 +1,60 @@
 #!/bin/bash
-
-##Update
+#Update
 apt-get install pwgen -y
 apt update && apt upgrade -y
 
-##HauptPakete lol
-apt install -y ca-certificates apt-transport-https lsb-release gnupg curl nano unzip apache2 mariadb-server mariadb-client php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4
-
-##PHPMyAdmin herunterladen und konfigurieren
+#HauptPakete lol
+apt install ca-certificates apt-transport-https lsb-release gnupg curl nano unzip -y
+wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add -
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+apt update
+apt install apache2 -y
+apt install php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4 -y
+apt install mariadb-server mariadb-client -y
+#mysql_secure_installation
 cd /usr/share
-wget -q https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip -O phpmyadmin.zip
-unzip phpmyadmin.zip && rm phpmyadmin.zip
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip -O phpmyadmin.zip
+unzip phpmyadmin.zip
+rm phpmyadmin.zip
 mv phpMyAdmin-*-all-languages phpmyadmin
 chmod -R 0755 phpmyadmin
 
-cat <<EOF >/etc/apache2/conf-available/phpmyadmin.conf
+#Vhost Erstellen
+echo -e '
 Alias /phpmyadmin /usr/share/phpmyadmin
 <Directory /usr/share/phpmyadmin>
-Options SymLinksIfOwnerMatch
-DirectoryIndex index.php
+    Options SymLinksIfOwnerMatch
+    DirectoryIndex index.php
 </Directory>
 <Directory /usr/share/phpmyadmin/templates>
-Require all denied
+    Require all denied
 </Directory>
 <Directory /usr/share/phpmyadmin/libraries>
-Require all denied
+    Require all denied
 </Directory>
 <Directory /usr/share/phpmyadmin/setup/lib>
-Require all denied
+    Require all denied
 </Directory>
-EOF
+' >> /etc/apache2/conf-available/phpmyadmin.conf
 
 a2enconf phpmyadmin
 systemctl reload apache2
 mkdir /usr/share/phpmyadmin/tmp/
 chown -R www-data:www-data /usr/share/phpmyadmin/tmp/
 
-##MySQL konfigurieren und User erstellen
-PASS=$(pwgen -s 40 1)
+#MySQL Konfigurieren und User Erstellen
+PASS=`pwgen -s 40 1`
 mysql <<MYSQL_SCRIPT
-CREATE USER 'fivem'@'localhost' IDENTIFIED BY '$PASS';
-GRANT ALL PRIVILEGES ON . TO 'fivem'@'localhost' WITH GRANT OPTION;
+CREATE USER 'root'@'localhost' IDENTIFIED BY '$PASS';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
 ip=$(hostname -i)
 
-##Installations-Log / Zugangsdaten erstellen
-cat <<EOF >/root/phpmyadmin-data.txt
-######### PHPMYADMIN Zugang #########
-Link: http://$ip/phpmyadmin
-User: fivem
-Passwort: $PASS
-EOF
+#InstallationsLog / Zugangsdaten Erstellen
+touch /root/phpmyadmin-data.txt
+echo -e "######### PHPMYADMIN Zugang #########" >> /root/phpmyadmin-data.txt
+echo -e "Link: http://"$ip"/phpmyadmin" >> /root/phpmyadmin-data.txt
+echo -e "User: root" >> /root/phpmyadmin-data.txt
+echo -e "Passwort: $PASS" >> /root/phpmyadmin-data.txt
